fd319719f315a8ccb6bd73694a6da0ad96b8cec7
Alessandro Vettor <ale02vettor@gmail.com>
2023-05-23 01:59:00 +0200

    0.0.0.0.4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <RouterView></RouterView>
+  <AuthButton />
 </template>
 
 <script>
+import AuthButton from '@/components/auth/AuthButton.vue';
+
 export default {
     name: 'App',
-    components: {}
+    components: {AuthButton}
 }
 </script>
 
